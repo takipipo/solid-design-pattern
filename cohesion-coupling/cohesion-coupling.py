be1b0299556638971f82ae73fa985ed03a2bb413
takipipo <ktphap.ncu@gmail.com>
2022-07-03 13:26:12 +0700

    Refactor 'VehicleRegisty' & 'Application' for less Coupling

--- cohesion-coupling/cohesion-coupling.py
+++ cohesion-coupling/cohesion-coupling.py
@@ -1,27 +1,49 @@
 import string
 import random
 
+
+class VehicleInfo:
+    def __init__(self, brand: str, catalogue_price: int, is_electric: bool) -> None:
+        self.brand = brand
+        self.catalogue_price = catalogue_price
+        self.is_electric = is_electric
+
+
+class Vehicle:
+    def __init__(self, vehicle_id: str, license_plate: str, info: VehicleInfo) -> None:
+        self.vehicle_id = vehicle_id
+        self.license_plate = license_plate
+        self.info = info
+
+
 class VehicleRegistry:
+    vehicle_info = {}
 
-    def generate_vehicle_id(self, length):
-        return ''.join(random.choices(string.ascii_uppercase, k=length))
+    def __init__(self) -> None:
+        self._add_vehicle_info("Tesla Model 3", True, 60000)
+        self._add_vehicle_info("Volkswagen ID3", False, 80000)
 
-    def generate_vehicle_license(self, id):
+    def _add_vehicle_info(self, brand: str, electric: bool, catalogue_price: int):
+        self.vehicle_info[brand] = VehicleInfo(brand, electric, catalogue_price)
+
+    def _generate_vehicle_id(self, length):
+        return "".join(random.choices(string.ascii_uppercase, k=length))
+
+    def _generate_vehicle_license(self, id):
         return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
 
+    def create_vehicle(self, brand: str) -> Vehicle:
+        vehicle_id = self._generate_vehicle_id(12)
+        license_plate = self._generate_vehicle_license(vehicle_id)
+
+        return Vehicle(vehicle_id, license_plate, self.vehicle_info[brand])
 
 class Application:
-
-    def register_vehicle(self, brand: string):
+    def register_vehicle(self, brand: str):
         # create a registry instance
         registry = VehicleRegistry()
 
-        # generate a vehicle id of length 12
-        vehicle_id = registry.generate_vehicle_id(12)
-
-        # now generate a license plate for the vehicle
-        # using the first two characters of the vehicle id
-        license_plate = registry.generate_vehicle_license(vehicle_id)
+        vehicle = registry.create_vehicle(brand)
 
         # compute the catalogue price
         catalogue_price = 0
@@ -46,6 +68,7 @@ class Application:
         print(f"Id: {vehicle_id}")
         print(f"License plate: {license_plate}")
         print(f"Payable tax: {payable_tax}")
+
 
 app = Application()
 app.register_vehicle("Volkswagen ID3")
