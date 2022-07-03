a138150a8aef24c615faebdc89378b544af2f644
takipipo <ktphap.ncu@gmail.com>
2022-07-03 13:55:04 +0700

    Fix incorrect argument order 'add_vehicle_info' & add compute_tax

--- cohesion-coupling/cohesion-coupling.py
+++ cohesion-coupling/cohesion-coupling.py
@@ -7,6 +7,14 @@ class VehicleInfo:
         self.brand = brand
         self.catalogue_price = catalogue_price
         self.is_electric = is_electric
+    
+    def compute_tax(self) -> float:
+        if self.is_electric:
+            tax_ratio = 0.02
+        else:
+            tax_ratio = 0.05
+        
+        return self.catalogue_price * tax_ratio
 
 
 class Vehicle:
@@ -20,8 +28,8 @@ class VehicleRegistry:
     vehicle_info = {}
 
     def __init__(self) -> None:
-        self._add_vehicle_info("Tesla Model 3", True, 60000)
-        self._add_vehicle_info("Volkswagen ID3", False, 80000)
+        self._add_vehicle_info("Tesla Model 3",60000, True,)
+        self._add_vehicle_info("Volkswagen ID3", 80000, False)
 
     def _add_vehicle_info(self, brand: str, electric: bool, catalogue_price: int):
         self.vehicle_info[brand] = VehicleInfo(brand, electric, catalogue_price)
@@ -42,31 +50,18 @@ class Application:
     def register_vehicle(self, brand: str):
         # create a registry instance
         registry = VehicleRegistry()
+        print(registry.vehicle_info)
 
         vehicle = registry.create_vehicle(brand)
 
-        # compute the catalogue price
-        catalogue_price = 0
-        if brand == "Tesla Model 3":
-            catalogue_price = 60000
-        elif brand == "Volkswagen ID3":
-            catalogue_price = 35000
-        elif brand == "BMW 5":
-            catalogue_price = 45000
-
-        # compute the tax percentage (default 5% of the catalogue price, except for electric cars where it is 2%)
-        tax_percentage = 0.05
-        if brand == "Tesla Model 3" or brand == "Volkswagen ID3":
-            tax_percentage = 0.02
-
         # compute the payable tax
-        payable_tax = tax_percentage * catalogue_price
+        payable_tax = vehicle.info.compute_tax()
 
         # print out the vehicle registration information
         print("Registration complete. Vehicle information:")
         print(f"Brand: {brand}")
-        print(f"Id: {vehicle_id}")
-        print(f"License plate: {license_plate}")
+        print(f"Id: {vehicle.vehicle_id}")
+        print(f"License plate: {vehicle.license_plate}")
         print(f"Payable tax: {payable_tax}")
 
 
